f9313ed362eb3626aac01c6a27bcbd0e6bd6283f
millennialmoron <millennialmoron87@gmail.com>
2022-08-17 22:31:51 +0200

    fix file paths after restructure

--- server/server.js
+++ server/server.js
@@ -238,7 +238,7 @@ async function main() {
   }
 
   app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
   });
 
   app.get("/list", function (req, res) {
