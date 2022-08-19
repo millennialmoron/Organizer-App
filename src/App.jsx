27f30a6bb18671145b1bd02c71a99cebbe125f31
millennialmoron <millennialmoron87@gmail.com>
2022-08-19 16:06:52 +0200

    changed script tag placement

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { Login } from "./components/Login";
 import { MainApp } from "./components/MainApp";
 
+//start fresh by commenting out logic and just pulling up main
+
 export default function App(props) {
   const [checkLogin, setCheckLogin] = useState(false);
 
