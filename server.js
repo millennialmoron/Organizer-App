34ea267e0cc708c17ca2018877702262e0294494
millennialmoron <millennialmoron87@gmail.com>
2022-08-20 15:57:33 +0200

    trying a different get file

--- server.js
+++ server.js
@@ -237,7 +237,7 @@ async function main() {
   }
 
   app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "client"));
   });
 
   app.get("/list", function (req, res) {
